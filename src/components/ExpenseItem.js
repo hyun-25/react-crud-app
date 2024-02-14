9f384ac282397d7e9b31ac067e32a2c6cd8faebe
hyun-25 <pingu0525@naver.com>
2024-02-14 21:46:40 +0900

    update

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import './ExpenseItem.css'
+import {MdDelete,MdEdit} from 'react-icons/md';
+
+export default class ExpenseItem extends Component {
+  render() {
+    return (
+      <div>
+        <li className='item'>
+            <div className='info'>
+             <span className='expense'>빵</span>
+             <span className='amount'>100원</span>
+            </div>
+
+        <div>
+            <button
+            className='edit-btn'
+            >
+                < MdEdit/>
+                
+            </button>
+            <button
+            className='clear-btn'
+            >
+                <MdDelete />
+            </button>
+        </div>
+        </li>
+      </div>
+    )
+  }
+}
